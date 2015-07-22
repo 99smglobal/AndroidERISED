f079383254af7bc3210455f85aaf3fc3d655a4cc
shahsurajk <shah.suraj.k@gmail.com>
2015-07-22 16:31:52 +0530

    Completed MainActivity XML

--- app/src/main/java/in/erised/android/erised/Men.java
+++ app/src/main/java/in/erised/android/erised/Men.java
@@ -10,6 +10,7 @@ public class Men extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.skeleton_grid);
     }
