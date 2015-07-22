2b449088270a21d3563ec033599ede4327a4d1b6
ncc486 <ncc486@gmail.com>
2015-07-22 14:45:18 +0530

    I have added the erised logo

---
+++ app/src/main/java/in/erised/android/erised/Men.java
@@ -0,0 +1,15 @@
+package in.erised.android.erised;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Akshay on 22/07/15.
+ */
+public class Men extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}
