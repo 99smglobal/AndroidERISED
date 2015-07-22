beef055ca7600de87cb879f84fca32125e9b8c1b
ncc486 <ncc486@gmail.com>
2015-07-22 15:03:21 +0530

    Some changes in manifest file .. Kids, women and Men java classes(activities) created.

---
+++ app/src/main/java/in/erised/android/erised/Kids.java
@@ -0,0 +1,16 @@
+package in.erised.android.erised;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Akshay on 22/07/15.
+ */
+public class Kids extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.skeleton_grid);
+    }
+}
---
+++ app/src/main/java/in/erised/android/erised/Women.java
@@ -0,0 +1,16 @@
+package in.erised.android.erised;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Akshay on 22/07/15.
+ */
+public class Women extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.skeleton_grid);
+    }
+}
